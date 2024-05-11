850d4154620f70fbf592da00ca403303a4f8b1ce
diegolds <diegolopes087@gmail.com>
2024-05-11 19:53:29 -0400

    adicionando prisma-adapter

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -4,13 +4,18 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
+
 
 export default function AuthForm() {
   const form = useForm()
 
-  const handleSubmit = form.handleSubmit(data => {
+  const handleSubmit = form.handleSubmit(async data => {
     console.log(data)
+    await signIn('email', {email: data.email})
   })
+
+
   return (
     <div className="flex h-screen w-full items-center justify-center bg-gray-100 px-4 dark:bg-gray-900">
       <div className="w-full max-w-md space-y-6">
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,13 +1,18 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
-
+import EmailProvider from 'next-auth/providers/email'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from '../database'
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub
+    EmailProvider({
+      server: process.env.EMAIL_SERVER, 
+      from: process.env.EMAIL_FROM
+    })
   ],
+  secret: process.env.SECRET
 })
